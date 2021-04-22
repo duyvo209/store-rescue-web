9574da7ab467657d9e7e42d9202cc7fd691500d6
Duy Vo <voduy200999@gmail.com>
2021-04-22 21:57:26 +0700

    crud

--- src/components/Service/index.jsx
+++ src/components/Service/index.jsx
@@ -1,78 +1,151 @@
-import * as React from 'react';
-import { DataGrid } from '@material-ui/data-grid';
+
+import React from 'react';
+import MaterialTable from 'material-table'
+import { forwardRef } from 'react';
+import AddBox from '@material-ui/icons/AddBox';
+import ArrowDownward from '@material-ui/icons/ArrowDownward';
+import Check from '@material-ui/icons/Check';
+import ChevronLeft from '@material-ui/icons/ChevronLeft';
+import ChevronRight from '@material-ui/icons/ChevronRight';
+import Clear from '@material-ui/icons/Clear';
+import DeleteOutline from '@material-ui/icons/DeleteOutline';
+import Edit from '@material-ui/icons/Edit';
+import FilterList from '@material-ui/icons/FilterList';
+import FirstPage from '@material-ui/icons/FirstPage';
+import LastPage from '@material-ui/icons/LastPage';
+import Remove from '@material-ui/icons/Remove';
+import SaveAlt from '@material-ui/icons/SaveAlt';
+import Search from '@material-ui/icons/Search';
+import ViewColumn from '@material-ui/icons/ViewColumn';
 import { db, auth } from '../../firebase';
-import { Button } from '@material-ui/core';
 
 
-const columns = [
-  { field: 'id', headerName: 'ID', width: 200 },
-  { field: 'name', headerName: 'Tên', width: 250 },
-  { field: 'desc', headerName: 'Mô tả', width: 350 },
-  {
-    field: 'price',
-    headerName: 'Giá',
-    type: 'number',
-    width: 150,
-  },
-  { field: 'deleteService', headerName: ' ', width: 150 },
- 
-];
+const tableIcons = {
+    Add: forwardRef((props, ref) => <AddBox {...props} ref={ref} />),
+    Check: forwardRef((props, ref) => <Check {...props} ref={ref} />),
+    Clear: forwardRef((props, ref) => <Clear {...props} ref={ref} />),
+    Delete: forwardRef((props, ref) => <DeleteOutline {...props} ref={ref} />),
+    DetailPanel: forwardRef((props, ref) => <ChevronRight {...props} ref={ref} />),
+    Edit: forwardRef((props, ref) => <Edit {...props} ref={ref} />),
+    Export: forwardRef((props, ref) => <SaveAlt {...props} ref={ref} />),
+    Filter: forwardRef((props, ref) => <FilterList {...props} ref={ref} />),
+    FirstPage: forwardRef((props, ref) => <FirstPage {...props} ref={ref} />),
+    LastPage: forwardRef((props, ref) => <LastPage {...props} ref={ref} />),
+    NextPage: forwardRef((props, ref) => <ChevronRight {...props} ref={ref} />),
+    PreviousPage: forwardRef((props, ref) => <ChevronLeft {...props} ref={ref} />),
+    ResetSearch: forwardRef((props, ref) => <Clear {...props} ref={ref} />),
+    Search: forwardRef((props, ref) => <Search {...props} ref={ref} />),
+    SortArrow: forwardRef((props, ref) => <ArrowDownward {...props} ref={ref} />),
+    ThirdStateCheck: forwardRef((props, ref) => <Remove {...props} ref={ref} />),
+    ViewColumn: forwardRef((props, ref) => <ViewColumn {...props} ref={ref} />)
+  };
 
-function createData(id, name, desc, price, deleteService) {
-  return {id, name, desc, price, deleteService};
-}
+  
 
+  function ClassTable() {
+    const { useState, useEffect } = React;
 
-export default function Service() {
-  const [user, setUser] = React.useState('');
-  const [rows, setRows] = React.useState([]);
+    const [columns, setColumns] = useState([
+      { title: 'ID', field: 'id', editable: 'never'},
+      { title: 'Tên dịch vụ', field: 'name' },
+      { title: 'Giá', field: 'price' },
+      { title: 'Mô tả', field: 'desc' },
 
-  // const handleDelete = (id) => {
-  //   return <Button variant="contained" color="primary" onClick={() => deleteService(id)}>Xoá</Button>
-  // }
+    ]);
 
-  // const deleteService = async (uid, id) => {
-  //   await db.collection('store').doc(uid).collection('service').doc(id).delete();
-  //   getData();
-  // }
+    
+    const [data, setData] = useState([]);
+    const [user, setUser] = React.useState('');
 
-  const getData = (uid) => {
-    console.log(uid);
-    db.collection('store').doc(uid).collection('service').get().then( snapshot => {
-      const service = [];
-      snapshot.forEach(doc => {
-        const data = doc.data();
-        // const id = doc.id;
-        service.push(data);
+    const getData = (uid) => {
+      console.log(uid);
+      db.collection('store').doc(uid).collection('service').get().then( async snapshot => {
+        const service = [];
+        snapshot.forEach(doc => {
+          const data = doc.data();
+          const id = doc.id;
+          service.push({
+            ...data,
+            uid: id
+          });
+        });
+        const newService = await service.map((item,index) => {
+          return {
+            ...item,
+            id: index+1
+          }
+        })
+        setData(newService)
+        
       });
-      console.log(service);
-      service.map((item,index) => {
-          setRows(previous => [
-              ...previous,
-              createData(index + 1, item.name, item.desc, item.price + ' VNĐ'),
-          ])
+    }
+  
+    const authListener = () => {
+      auth.onAuthStateChanged((user) => {
+          if (user) {
+              getData(user.uid)
+              setUser(user);
+          } else {
+              setUser('');
+          }
       });
-    });
+    }
+  
+    React.useEffect(() => {
+      authListener();
+    },[]);
+    
+   
+    return (
+      <MaterialTable
+        icons={tableIcons}
+        title="Quản lý dịch vụ"
+        columns={columns}
+        data={data}
+        editable={{
+          onRowAdd: newData =>
+            new Promise((resolve, reject) => {
+              setTimeout(() => {
+                setData([...data, newData]);
+                db.collection('store').doc(auth.currentUser.uid).collection('service').add({
+                  'name': newData.name,
+                  'price': newData.price,
+                  'desc': newData.desc,
+                });
+                resolve();
+              }, 1000)
+            }),
+          onRowUpdate: (newData, oldData) =>
+            new Promise((resolve, reject) => {
+              setTimeout(() => {
+                const dataUpdate = [...data];
+                const index = oldData.tableData.id;
+                dataUpdate[index] = newData;
+                setData([...dataUpdate]);
+                db.collection('store').doc(auth.currentUser.uid).collection('service').doc(newData.uid).update({
+                  'name': newData.name,
+                  'price': newData.price,
+                  'desc': newData.desc,
+                });
+                resolve();
+              }, 1000)
+            }),
+          onRowDelete: oldData =>
+            new Promise((resolve, reject) => {
+              setTimeout(() => {
+                const dataDelete = [...data];
+                const index = oldData.tableData.id;
+                dataDelete.splice(index, 1);
+                setData([...dataDelete]);
+                  console.log(oldData.uid);
+                db.collection('store').doc(auth.currentUser.uid).collection('service').doc(oldData.uid).delete();
+                
+                resolve()
+              }, 1000)
+            }),
+        }}
+      />
+    )
   }
-
-  const authListener = () => {
-    auth.onAuthStateChanged((user) => {
-        if (user) {
-            getData(user.uid)
-            setUser(user);
-        } else {
-            setUser('');
-        }
-    });
-  }
-
-  React.useEffect(() => {
-    authListener();
-  },[]);
-
-  return (
-    <div style={{ height: 400, width: '100%' }}>
-      <DataGrid rows={rows} columns={columns} pageSize={5} checkboxSelection />
-    </div>
-  );
-}
+  
+export default ClassTable;
--- src/pages/Service.jsx
+++ src/pages/Service.jsx
@@ -1,13 +1,8 @@
 import ServiceComponents from '../components/Service';
-import Button from '@material-ui/core/Button';
-import AddService from '../components/AddService'
 
 const Service = () => {
     return (
         <div>
-            {/* <Button variant="contained">Thêm dịch vụ</Button> */}
-            <AddService />
-            <br/>
             <ServiceComponents />
         </div>
     );
